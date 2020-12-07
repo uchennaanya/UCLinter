a6b13469032b937c599abb1e867af64d5c5937a7
uchennaanya <uchennaongit@gmail.com>
2020-12-07 21:16:09 +0100

    Added a class

--- bin/main.rb
+++ bin/main.rb
@@ -1,18 +1,27 @@
 #!/usr/bin/env ruby
 require_relative '../lib/check.rb'
-
-check = CheckError.new(ARGV.first)
-check.check_trailing_spaces
-check.tag_error
-check.end_error
-check.empty_line_error
-
-if check.errors.empty? && check.checker.err_msg.empty?
-  puts 'No offenses'.colorize(:green) + ' detected '
-else
-  check.errors.uniq.each do |err|
-    puts "#{check.checker.file_path.colorize(:blue)} : #{err.colorize(:red)}"
+class UcLinter
+  attr_accessor :check
+  def initialize
+    self.check = CheckError.new(ARGV.first)
   end
+  def checker
+    check.check_trailing_spaces
+    check.tag_error
+    check.end_error
+    check.empty_line_error
+
+    if check.errors.empty? && check.checker.err_msg.empty?
+      puts 'No offenses'.colorize(:green) + ' detected '
+    else
+      check.errors.uniq.each do |err|
+        puts "#{check.checker.file_path.colorize(:blue)} : #{err.colorize(:red)}"
+      end
+    end
+    puts check.checker.err_msg if check.checker.file_lines.empty?
+  end  
 end
 
-puts check.checker.err_msg if check.checker.file_lines.empty?
+uclinter = UcLinter.new
+
+uclinter.checker
